a743d2a9b237f6ca01ef6db11bdea6f1dbbfcbb8
Thiago Franco <thiago.boo@gmail.com>
2018-11-06 09:46:19 -0200

    Criação do Projeto

---
+++ AulaDelegate/AulaDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Course.Services;
+
+namespace AulaDelegate
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            double result = CalculationService.Square(a);
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
